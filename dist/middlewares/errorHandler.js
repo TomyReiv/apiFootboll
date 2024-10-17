7c87075e1f013dc45de6d1ff4d6af3fed8ba6584
Tomas <ravetomas@gmail.com>
2024-10-17 12:09:08 +0200

    First commit

--- dist/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = errorHandler;
-function errorHandler(err, req, res, next) {
-    console.error('ERROR:', err.stack);
-    res.status(500).json({ message: err.message });
-}
-//# sourceMappingURL=errorHandler.js.map
